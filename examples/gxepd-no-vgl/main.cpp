7b72a0ad84d9773ff28e44ecaacd72ccaa61da07
martinberlin <martin@luckycloud.de>
2020-09-27 12:07:09 +0200

    Organize demos in examples/ directory. This directory can be select uncommenting one line in platformio.ini [platformio] section

--- examples/gxepd-no-vgl/main.cpp
+++ examples/gxepd-no-vgl/main.cpp
@@ -15,6 +15,7 @@
 GxIO_Class io(SPI, EINK_CS, EINK_DC, EINK_RST);
 // (GxIO& io, uint8_t rst = D4, uint8_t busy = D2);
 GxEPD_Class display(io, EINK_RST, EINK_BUSY );
+bool printClearScreenButton = true;
 
 // I2C Touch pins
 int i2c_sda = I2C_SDA;
@@ -23,36 +24,50 @@ uint32_t i2c_freq = I2C_FREQ;
 // INTGPIO is touch interrupt, goes low when it detects a touch, which coordinates are read by I2C
 FT6X36 ts(&Wire, INTGPIO);
 
+void drawClearButton(){
+    display.fillRoundRect(1,display.height()-40,40,40,4,GxEPD_BLACK);
+    display.setCursor(5,display.height()-10);
+    display.setTextColor(GxEPD_WHITE);
+    display.print("CLEAR");
+    display.setTextColor(GxEPD_BLACK);
+}
 
 void printTouchInfo(TPoint p, TEvent e)
 {
-  Serial.printf("printTouchInfo callback\n");
   if (e != TEvent::Tap && e != TEvent::DragStart && e != TEvent::DragMove && e != TEvent::DragEnd)
     return;
 
-  Serial.print("X: ");
-  Serial.print(p.x);
-  Serial.print(", Y: ");
-  Serial.print(p.y);
-  Serial.print(", E: ");
-
+  std::string eventName = "";
   switch (e)
   {
   case TEvent::Tap:
-    Serial.println("Tap");
+    eventName = "tap";
     break;
   case TEvent::DragStart:
-    Serial.println("DragStart");
+    eventName = "DragStart";
     break;
   case TEvent::DragMove:
-    Serial.println("DragMove");
+    eventName = "DragMove";
     break;
   case TEvent::DragEnd:
-    Serial.println("DragEnd");
+    eventName = "DragEnd";
     break;
   default:
-    Serial.println("UNKNOWN");
+    eventName = "UNKNOWN";
     break;
+  }
+
+  Serial.printf("X: %d Y: %d E: %s\n", p.x, p.y, eventName.c_str());
+
+  if (p.x<25 && p.y>200) { // Clear screen
+    display.fillScreen(GxEPD_WHITE);
+    drawClearButton();
+    display.update();
+  } else {
+    // Print coordinates
+    display.setCursor(p.x, p.y+10);
+    display.printf("X:%d Y:%d E:%s", p.x, p.y, eventName.c_str());
+    display.updateWindow(p.x, p.y, display.width(), 20);
   }
 }
 
@@ -72,8 +87,17 @@ void setup()
   Wire.begin(i2c_sda,i2c_sdl,i2c_freq); // Defined in platformio.ini
   //Wire.begin(); // Will start I2C on default pins default speed
   display.init();
-  display.fillRoundRect(20,20,100,50,4,GxEPD_BLACK);
+  //display.setRotation(1); // This won't work because it should be Touch rotation aware
+  
+  // Optional font
+  //display.setFont(&FreeMono9pt7b);
+  if (printClearScreenButton) {
+    drawClearButton();
+  }
   display.update();
+  
+  
+  
 
   ts.begin();
   ts.registerTouchHandler(touch);
